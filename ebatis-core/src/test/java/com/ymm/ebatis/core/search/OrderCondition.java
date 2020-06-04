608ea47968429df7914834467a179bdc09d1e58c
章多亮 <duoliang.zhang@amh-group.com>
2020-06-04 10:00:10 +0800

    增加一个前缀注解，这样可以将嵌套的映射字段很好的组织在一起

--- ebatis-core/src/test/java/com/ymm/ebatis/core/search/OrderCondition.java
+++ ebatis-core/src/test/java/com/ymm/ebatis/core/search/OrderCondition.java
@@ -1,7 +1,10 @@
 package com.ymm.ebatis.core.search;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ymm.ebatis.core.annotation.Field;
+import com.ymm.ebatis.core.annotation.Filter;
 import com.ymm.ebatis.core.annotation.MustNot;
+import com.ymm.ebatis.core.annotation.QueryType;
 import lombok.Data;
 
 import java.util.Date;
@@ -13,6 +16,13 @@ public class OrderCondition {
     private Long orderId;
     @JsonProperty("order_date")
     private Date orderDate;
+    @Filter(queryType = QueryType.MATCH)
+    private String category;
+
+    @Field("manufacturer")
+    private String[] manufacturers;
+
+    private ProductCondition[] products;
 
     private CustomerCondition customerCondition;
 }
