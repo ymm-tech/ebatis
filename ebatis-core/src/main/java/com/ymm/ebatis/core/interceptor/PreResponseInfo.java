0595396cbfa92d2a64f36da8feae8e261e2bb07d
weilong.hu <weilong.hu@amh-group.com>
2020-06-23 16:13:29 +0800

    1.增加拦截器，请求前和请求后处理

--- ebatis-core/src/main/java/com/ymm/ebatis/core/interceptor/PreResponseInfo.java
+++ ebatis-core/src/main/java/com/ymm/ebatis/core/interceptor/PreResponseInfo.java
@@ -2,19 +2,18 @@ package com.ymm.ebatis.core.interceptor;
 
 import com.ymm.ebatis.core.response.ResponseExtractor;
 import org.elasticsearch.action.ActionRequest;
-import org.elasticsearch.action.ActionResponse;
 
 /**
  * @author weilong.hu
  * @date 2020-04-21
  */
-public interface PreResponseInfo<T extends ActionRequest, R extends ActionResponse> {
+public interface PreResponseInfo<T extends ActionRequest> {
     /**
-     * @return
+     * 返回ActionRequest
+     *
+     * @return actionRequest
      */
     T actionRequest();
-
-    Class<R> responseClass();
 
     ResponseExtractor<?> extractor();
 }
