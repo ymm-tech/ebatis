de2fff2b8c614d2f010e73768d09bc43e7a58ac1
weilong.hu <weilong.hu@amh-group.com>
2020-07-11 13:50:45 +0800

    1.注释调整

--- ebatis-sample/src/test/java/com/ymm/ebatis/sample/mapper/RecentOrderGetMapper.java
+++ ebatis-sample/src/test/java/com/ymm/ebatis/sample/mapper/RecentOrderGetMapper.java
@@ -14,31 +14,66 @@ import java.util.concurrent.CompletableFuture;
  */
 @EasyMapper(indices = "recent_order_index")
 public interface RecentOrderGetMapper {
-    //Get Entity Long
+    /**
+     * 查询订单
+     *
+     * @param id 文档id
+     * @return 订单 可能为null
+     */
     @Get
     RecentOrder getRecentOrder(Long id);
 
-    //Get Entity String
+    /**
+     * 查询订单
+     *
+     * @param id 文档id
+     * @return 订单 可能为null
+     */
     @Get
     RecentOrder getRecentOrder(String id);
 
-    //Get Entity Model
+    /**
+     * 查询订单
+     *
+     * @param order 订单，需实现IdProvider
+     * @return 订单 可能为null
+     */
     @Get
-    RecentOrder getRecentOrder(RecentOrderModel recentOrderModel);
+    RecentOrder getRecentOrder(RecentOrderModel order);
 
-    //Get Optional<Entity> Long
+    /**
+     * 查询订单
+     *
+     * @param id 订单id
+     * @return Option
+     */
     @Get
     Optional<RecentOrder> getRecentOrderOptional(Long id);
 
-    //Get GetResponse Long
+    /**
+     * 查询订单
+     *
+     * @param id 订单id
+     * @return GetResponse
+     */
     @Get
     GetResponse getRecentOrderGetResponse(Long id);
 
-    //Get CompletableFuture<Optional<Entity>> Long
+    /**
+     * 查询订单
+     *
+     * @param id 订单id
+     * @return 异步Optional
+     */
     @Get
     CompletableFuture<Optional<RecentOrder>> getRecentOrderCompletableFuture(Long id);
 
-    //Get CompletableFuture<Entity> String
+    /**
+     * 查询订单
+     *
+     * @param id 订单id
+     * @return 异步订单结果
+     */
     @Get
     CompletableFuture<RecentOrder> getRecentOrderCompletableFuture(String id);
 }
