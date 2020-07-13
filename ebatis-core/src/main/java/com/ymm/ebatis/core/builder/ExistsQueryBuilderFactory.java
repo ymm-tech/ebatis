4007ef54e36c66050aa359ebe648445ed0d7ba92
weilong.hu <weilong.hu@amh-group.com>
2020-07-13 18:54:44 +0800

    1.Exists调整

--- ebatis-core/src/main/java/com/ymm/ebatis/core/builder/ExistsQueryBuilderFactory.java
+++ ebatis-core/src/main/java/com/ymm/ebatis/core/builder/ExistsQueryBuilderFactory.java
@@ -1,12 +1,11 @@
 package com.ymm.ebatis.core.builder;
 
 import com.ymm.ebatis.core.annotation.Exists;
+import com.ymm.ebatis.core.exception.ConditionNotSupportException;
 import com.ymm.ebatis.core.meta.ConditionMeta;
 import org.elasticsearch.index.query.ExistsQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-
-import java.util.Optional;
 
 /**
  * @author 章多亮
@@ -19,18 +18,16 @@ class ExistsQueryBuilderFactory extends AbstractQueryBuilderFactory<QueryBuilder
     }
 
     @Override
-    protected boolean onlyHandleNoneNullable() {
-        return false;
-    }
-
-    @Override
     protected QueryBuilder doCreate(ConditionMeta meta, Object condition) {
-        Optional<Exists> exists = meta.findAttributeAnnotation(Exists.class);
+        if (Boolean.TYPE != meta.getType() && Boolean.class != meta.getType()) {
+            throw new ConditionNotSupportException(meta.getName() + ":Exists query must be boolean or Boolean!");
+        }
+        Boolean bool = (Boolean) condition;
         ExistsQueryBuilder existsQuery = QueryBuilders.existsQuery(meta.getName());
-        if (exists.isPresent()) {
-            return exists.get().value() ? existsQuery : QueryBuilders.boolQuery().mustNot(existsQuery);
-        } else {
+        if (bool) {
             return existsQuery;
+        } else {
+            return QueryBuilders.boolQuery().mustNot(existsQuery);
         }
     }
 }
