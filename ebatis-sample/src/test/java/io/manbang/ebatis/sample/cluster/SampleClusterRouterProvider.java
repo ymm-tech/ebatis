796bff112023cfae16a0b4d333d3dc70d6752a5c
weilong.hu <weilong.hu@amh-group.com>
2020-07-31 09:30:53 +0800

    1.测试调整

--- ebatis-sample/src/test/java/io/manbang/ebatis/sample/cluster/SampleClusterRouterProvider.java
+++ ebatis-sample/src/test/java/io/manbang/ebatis/sample/cluster/SampleClusterRouterProvider.java
@@ -4,7 +4,6 @@ import com.google.auto.service.AutoService;
 import io.manbang.ebatis.core.cluster.Cluster;
 import io.manbang.ebatis.core.cluster.ClusterRouter;
 import io.manbang.ebatis.core.cluster.ClusterRouterProvider;
-import io.manbang.ebatis.core.cluster.Credentials;
 
 /**
  * @author weilong.hu
@@ -17,8 +16,7 @@ public class SampleClusterRouterProvider implements ClusterRouterProvider {
     @Override
     public ClusterRouter getClusterRouter(String name) {
         if (SAMPLE_CLUSTER_NAME.equalsIgnoreCase(name)) {
-            Cluster cluster = Cluster.simple("192.168.199.7", 9200, Credentials.basic("stevedore", "yV8jwR5omQ"));
-            return ClusterRouter.single(cluster);
+            return ClusterRouter.single(Cluster.localhost());
 
         } else {
             return null;
